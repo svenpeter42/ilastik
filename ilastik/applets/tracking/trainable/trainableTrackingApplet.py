af7d05c8036111593b1c623e0c638142a5c7f0d0
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2013-07-09 16:23:51 +0200

    trainableTracking: firing up isolated applet for further development

--- ilastik/applets/tracking/trainable/trainableTrackingApplet.py
+++ ilastik/applets/tracking/trainable/trainableTrackingApplet.py
@@ -23,3 +23,56 @@ class TrainableTrackingApplet( StandardApplet ):
     @property
     def dataSerializers(self):
         return self._serializableItems
+
+if __name__ == "__main__":
+    from PyQt4.QtGui import QApplication
+
+    #make the program quit on Ctrl+C
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    from ilastik.workflow import Workflow
+    from ilastik.shell.projectManager import ProjectManager
+    from lazyflow.operator import Operator
+    from lazyflow.slot import OutputSlot, InputSlot
+    from lazyflow.graph import Graph
+
+    class ImageNameOperator( Operator ):
+        Output = OutputSlot( level=1 )
+
+        def setupOutputs( self ):
+            self.Output.setValue("Dummy Name")
+
+        def execute( self, *args, **kwargs ):
+            pass
+
+    class DummyWorkflow( Workflow ):
+        @property
+        def applets( self ):
+            return self._applets
+
+        @property
+        def imageNameListSlot( self ):
+            return self._opImageName.Output
+
+
+        def __init__(self, headless=False, workflow_cmdline_args=(), parent=None, graph=None):
+            super(DummyWorkflow, self).__init__(headless, workflow_cmdline_args, parent, graph)
+            self._applets = [ TrainableTrackingApplet( workflow=self ) ]
+            self._opImageName = ImageNameOperator( parent = self )
+
+        def connectLane( self, laneIndex ):
+            pass
+
+        def onProjectLoaded( self, projectManager ):
+            self._opImageName.Output.resize(1)
+
+if __name__=="__main__":            
+    def createNewProject(shell):
+        shell.createAndLoadNewProject("tmp.ilp", DummyWorkflow, h5_file_kwargs={'driver': 'core', 'backing_store': False})
+    init_funcs = [createNewProject]
+
+    from ilastik.shell.gui.startShellGui import startShellGui
+    import sys
+    sys.exit(startShellGui({}, False, *init_funcs))
+    
--- ilastik/applets/tracking/trainable/trainableTrackingGui.py
+++ ilastik/applets/tracking/trainable/trainableTrackingGui.py
@@ -11,11 +11,23 @@ logger = logging.getLogger(__name__)
 traceLogger = logging.getLogger('TRACE.' + __name__)
 
 class TrainableTrackingGui( LayerViewerGui ):
-    def _loadUiFile(self):
+    pass
+    # def _loadUiFile(self):
+    #     # Load the ui file (find it in our own directory)
+    #     localDir = os.path.split(__file__)[0]
+    #     self._drawer = uic.loadUi(localDir+"/drawer.ui")        
+    #     return self._drawer
+    
+    # def initAppletDrawerUi(self):
+    #     super(TrainableTrackingGui, self).initAppletDrawerUi()
+    #     print "ASDFASDF", self._drawer 
+        
+    def initAppletDrawerUi(self):
+        """
+        By default, this base class provides a blank applet drawer.
+        Override this in a subclass to get a real applet drawer.
+        """
         # Load the ui file (find it in our own directory)
         localDir = os.path.split(__file__)[0]
-        self._drawer = uic.loadUi(localDir+"/drawer.ui")        
-        return self._drawer
-    
-    def initAppletDrawerUi(self):
-        super(TrainableTrackingGui, self).initAppletDrawerUi()    
+        self._drawer = uic.loadUi(localDir+"/drawer.ui")
+
